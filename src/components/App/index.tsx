2d8b6eb3f8439f90a5a8fd61dce357a6c257e472
Caio <whoiscaio@protonmail.com>
2022-01-19 11:25:48 -0300

    feat: theme switcher setup completed

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
 import GlobalStyle from '../../styles/GlobalStyle';
+import dark from '../../styles/themes/dark';
+import light from '../../styles/themes/light';
 
 function App() {
+  const [theme, setTheme] = useState<string>('light');
+
+  function toggleTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+
   return (
     <div className="App">
       <GlobalStyle />
+      <ThemeProvider theme={theme === 'light' ? light : dark}>
+        
+      </ThemeProvider>
     </div>
   );
 }
