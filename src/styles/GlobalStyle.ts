78f92f1fe8272bce8cd7f8054ac76cea27f32fde
Caio <whoiscaio@protonmail.com>
2022-01-19 12:02:08 -0300

    feat: font size and family pattern added

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -4,7 +4,11 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    font-family: sans-serif;
+    font-family: 'Nunito Sans', sans-serif;
     box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
   }
 `;
