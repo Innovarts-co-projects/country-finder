d4930f5f21a1336c9cd50bf64e41357c13e7ce4e
Caio <whoiscaio@protonmail.com>
2022-01-21 08:09:54 -0300

    feat: home page stylization adjusted to fit the country card properly

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
 
 const HomePageContainer = styled.main`
-  
+  width: 100%;
+  max-width: 1440px;
+
+  margin: 0 auto;
+  padding: 0 5rem;
+
+  .countries {
+    display: flex;
+    justify-content: space-between;
+
+    flex-wrap: wrap;
+  }
 `;
 
 export { HomePageContainer };
