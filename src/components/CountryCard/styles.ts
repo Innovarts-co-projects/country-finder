993eca0836940aa1d23969c78cfee2dfcaf7fbfc
Caio <whoiscaio@protonmail.com>
2022-01-21 10:06:39 -0300

    feat: country card styles updated

--- src/components/CountryCard/styles.ts
+++ src/components/CountryCard/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const CountryCardContainer = styled.div`
-  background: ${({ theme }) => theme.background};
+  background: ${({ theme }) => theme.elements};
   border-radius: .8rem;
 
   width: 21%;
@@ -9,10 +9,12 @@ const CountryCardContainer = styled.div`
   margin-bottom: 4rem;
 
   box-shadow: 3px 2px 8px 2px rgba(0,0,0,0.53);
+  transition: transform .24s ease-out;
 
   .image {
     img {
-      border-radius: .8rem;
+      border-top-left-radius: .8rem;
+      border-top-right-radius: .8rem;
 
       width: 100%;
     }
@@ -21,17 +23,28 @@ const CountryCardContainer = styled.div`
   .info {
     color: ${({ theme }) => theme.text};
 
-    padding: 1.2rem;
-    padding-bottom: 5rem;
+    padding: 2rem;
+    padding-bottom: 3rem;
 
     h2 {
       font-size: 1.8rem;
-      margin-bottom: .8rem;
+      margin-bottom: 1rem;
     }
 
     p {
       font-size: 1.3rem;
+      font-weight: 300;
+
+      margin-bottom: .4rem;
+
+      strong {
+        font-weight: 600;
+      }
     }
+  }
+
+  &:hover {
+    transform: scale(1.05);
   }
 `;
 
