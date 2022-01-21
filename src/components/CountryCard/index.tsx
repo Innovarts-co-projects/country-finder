770434d38aacf6d2d3308d6962309aef3b53be2b
Caio <whoiscaio@protonmail.com>
2022-01-21 07:42:07 -0300

    feat: country card components jsx structure added

--- src/components/CountryCard/index.tsx
+++ src/components/CountryCard/index.tsx
@@ -1,6 +1,26 @@
-function CountryCard() {
+import { Link } from 'react-router-dom';
+import { reqInterface } from '../../pages/Home/reqTypes';
+import { CountryCardContainer } from './styles';
+
+interface CountryCardProps {
+  country: reqInterface,
+}
+
+function CountryCard({ country }: CountryCardProps) {
   return (
-    <p>Country Card</p>
+    <CountryCardContainer>
+      <Link to={`country/${country.name}`}>
+        <div className="image">
+          <img src={country.flag} alt={`${country.name} flag`} />
+        </div>
+        <div className="info">
+          <h2>{country.name}</h2>
+          <p>Population: {country.population}</p>
+          <p>Region: {country.region}</p>
+          <p>Capital: {country.capital}</p>
+        </div>
+      </Link>
+    </CountryCardContainer>
   )
 }
 
